515b6d7fb67d88657d632938eade43ea2edf0111
Gioele Crispo <gioele.crispo@external.eni.com>
2021-11-19 20:34:19 +0100

    updated screen (bad path, remove "src"). Added rate_us webbrowser

--- src/uix/pages/home_screen.py
+++ src/uix/pages/home_screen.py
@@ -1,3 +1,6 @@
+import webbrowser
+from multiprocessing import Process
+
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.screen import MDScreen
@@ -7,6 +10,9 @@ class HomeScreen(MDScreen):
     rate_us_dialog = None
 
     def show_rate_us_dialog(self):
+
+
+
         if not self.rate_us_dialog:
             self.rate_us_dialog = MDDialog(
                 text="If you like this application, please give us five stars on Play Store",
@@ -14,8 +20,14 @@ class HomeScreen(MDScreen):
                 buttons=[
                     MDFlatButton(
                         text="Rate us",
-                        theme_text_color="Custom"
+                        theme_text_color="Custom",
+                        on_release=self.go_to_playstore
                     ),
                 ]
             )
         self.rate_us_dialog.open()
+
+    def go_to_play_store(self, inst):
+        webbrowser.open("market://details?id=com.amazon.mp3")
+        self.rate_us_dialog.dismiss()
+
