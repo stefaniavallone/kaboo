9346a65a2e4a3905b96640c7a350549aaf27f6c5
Crispo, Gioele <gioele.crispo@nttdata.com>
2022-02-19 14:03:50 +0100

    added ads for 1st player each round

--- src/uix/pages/game_pre_screen.py
+++ src/uix/pages/game_pre_screen.py
@@ -25,16 +25,17 @@ class GamePreScreen(MDScreen):
         self.num_jumps = str(self.app.status.getv("game.num_jumps", default_value=5))
         self.game_level = self.app.status.getv("game.level", default_value="easy")
         self.current_round = str(self.app.status.getv("game.current_round", default_value=0))
-        curr_player = self.app.status.getv("game.current_player", default_value=0)
-        self.points = str(self.app.status.getv(f"game.rounds.r{str(int(self.current_round)-1)}.p{str(curr_player)}.points", default_value="0"))
-        self.current_player = f"Team {str(curr_player + 1)}"
-        self.image = f"assets/images/pregame/your_turn_{str(curr_player)}.png"
+        self.curr_player = self.app.status.getv("game.current_player", default_value=0)
+        self.points = str(self.app.status.getv(f"game.rounds.r{str(int(self.current_round)-1)}.p{str(self.curr_player)}.points", default_value="0"))
+        self.current_player = f"Team {str(self.curr_player + 1)}"
+        self.image = f"assets/images/pregame/your_turn_{str(self.curr_player)}.png"
         self.title = self.app.i18n._("PREGAME_TEAM_TURN", team=self.current_player) # f"It's {self.current_player} turn!"
-        self.ids.play_button.md_bg_color = PLAYERS_COLORS[curr_player]
+        self.ids.play_button.md_bg_color = PLAYERS_COLORS[self.curr_player]
 
     def to_game(self):
-        self.app.ads.request_interstitial()
-        self.app.ads.show_interstitial()
+        if self.curr_player == 0:
+            self.app.ads.request_interstitial()
+            self.app.ads.show_interstitial()
         self.manager.go_to_screen('game', direction='left')
 
     def to_home(self):
@@ -42,4 +43,5 @@ class GamePreScreen(MDScreen):
         self.manager.go_to_screen('home', direction='right')
 
     def on_resume(self):
-        self.app.ads.request_interstitial()
+        if self.curr_player == 0:
+            self.app.ads.request_interstitial()
